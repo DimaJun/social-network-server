8bcf4c49ce38af8c375ae379759bf2ffb867264e
Dima Rui <drankendmitrij@gmail.com>
2025-12-21 14:46:58 +0200

    added: user operations, user dtos;

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(dto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: dto
+    });
+  }
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {email}
+    })
+  }
+}
