cede429a0a610c871a2c9b33c20e8e12d3e2c6f2
Dima Rui <drankendmitrij@gmail.com>
2025-12-21 19:34:19 +0200

    added: login flow;

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import type{Response} from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,22 @@ export class AuthController {
 	@Post('/register')
 	register(@Body() dto: RegisterDto){
 	  return this.authService.register(dto);
+	}
+
+	@Post('/login')
+	async login(@Body() dto: LoginDto, @Res({passthrough: true}) res: Response) {
+	  const data = await this.authService.login(dto);
+
+	  res.cookie('refresh', data.tokens.refresh_token, {
+		  httpOnly: true,
+		  secure: false,
+		  sameSite: "strict",
+		  maxAge: 2 * 24 * 60 * 60 * 1000,
+	  });
+
+	  return {
+		  user: data.user,
+		  access_token: data.tokens.access_token
+	  }
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from './dto/register.dto';
+import { JwtPayload } from './types/auth';
+import { LoginDto } from './dto/login.dto';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -17,5 +20,44 @@ export class AuthService {
 			const { password, ...withoutPassword } = user;
 			return withoutPassword;
 		}
+	}
+
+	async login(dto: LoginDto) {
+		const isUserExist = await this.userService.findUserByEmail(dto.email);
+		if(!isUserExist) {
+			throw new UnauthorizedException(`Пользователя с почтой: ${dto.email} не существует!`);
+		}
+		const isPasswordsMatch = await bcrypt.compare(dto.password, isUserExist.password);
+		if(!isPasswordsMatch) {
+			throw new UnauthorizedException('Неправильный пароль!')
+		}
+		const {id, username, email} = isUserExist;
+		const payload = {
+			id,
+			username,
+			email
+		}
+
+		return {
+			user: payload,
+			tokens: {
+				access_token: this.generateAccessToken(payload),
+				refresh_token: this.generateRefreshToken(payload)
+			}
+		}
+	}
+
+	generateAccessToken(payload: JwtPayload) {
+		return this.jwtService.sign(payload, {
+			secret: process.env.JWT_ACCESS_SECRET,
+			expiresIn: '15m'
+		})
+	}
+
+	generateRefreshToken(payload: JwtPayload) {
+		return this.jwtService.sign(payload, {
+			secret: process.env.JWT_REFRESH_SECRET,
+			expiresIn: '2d',
+		});
 	}
 }
